f9bf4f629c710681411be045acc9c17a4e5626ac
dregor <dregor614@bk.ru>
2017-02-10 17:39:01 +0300

    add triangles

--- Assets/scripts/maw.cs
+++ Assets/scripts/maw.cs
@@ -6,15 +6,13 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class maw : MonoBehaviour
 {
-    public uint layers = 2;
-    public uint angles = 3;
-    public float radiusOut = 2f;
-    public float radiusIn = 1f;
-    public float depth = 1f;
+    public uint layers = 1;
+    public uint angles = 5;
+    public float radiusOut = 20f;
+    public float radiusIn = 10f;
+    public float depth = 10f;
 
     public Color dotColor = new Color(150, 150, 150);
-    private Vector3[,] verticesIn;
-    private Vector3[,] verticesOut;
     private List<Vector3> allVertices = new List<Vector3>();
     private Mesh mesh;
 
@@ -35,57 +33,58 @@ public class maw : MonoBehaviour
             yield return vertice;
         }
     }
-    
+
     private IEnumerator Generate()
     {
-        verticesIn = new Vector3[layers, angles];
-        verticesOut = new Vector3[layers, angles];
-        uint i = 0;
+        mesh = new Mesh();
+        mesh.name = "polyhedron";
+        GetComponent<MeshFilter>().mesh = mesh;
+        int[] triangles = new int[layers * angles * 6];
+        uint index = 0;
         for (uint layer = 0; layer < layers; layer++)
-        {         
+        {
             foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusOut))
             {
-                verticesOut[layer, i] = vertice;
+                MakeAText(vertice, index.ToString());
                 allVertices.Add(vertice);
+                index++;
             }
             foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusIn))
             {
-                verticesIn[layer, i] = vertice;
+                MakeAText(vertice, index.ToString());
                 allVertices.Add(vertice);
+                index++;
             }
             mesh.vertices = allVertices.ToArray();
+
             yield return false;
 
-            i++;
         }
+        mesh.vertices = allVertices.ToArray();
 
-        //mesh.vertices = allVertices.ToArray();
+        for (int i = 0, j = 0; i < angles-1; i++)
+        {
+            triangles[j] = i;
+            triangles[j + 3] = triangles[j + 2] = i + (int)angles;
+            triangles[j + 4] = triangles[j + 1] = i + 1;
+            triangles[j + 5] = i + (int)angles +1;
+            j += 6;
+            mesh.triangles = triangles;
+            yield return false;
+        }
+        mesh.RecalculateNormals();
 
+    }
 
-
-        //vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        //for (int i = 0, y = 0; y <= ySize; y++)
-        //{
-        //    for (int x = 0; x <= xSize; x++, i++)
-        //    {
-        //        vertices[i] = new Vector3(x, y);
-        //    }
-        //}
-        //mesh.vertices = vertices;
-
-        //int[] triangles = new int[xSize * ySize * 6];
-        //for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
-        //{
-        //    for (int x = 0; x < xSize; x++, ti += 6, vi++)
-        //    {
-        //        triangles[ti] = vi;
-        //        triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-        //        triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-        //        triangles[ti + 5] = vi + xSize + 2;
-        //    }
-        //    mesh.triangles = triangles;
-        //    mesh.RecalculateNormals();
-        //}
+    private void MakeAText( Vector3 position, String str)
+    {
+        GameObject text = new GameObject("TextMesh: "+ str);
+        text.AddComponent<TextMesh>();
+        text.transform.position = position;
+        text.transform.parent = this.transform;
+        text.GetComponent<TextMesh>().text = str;
+        text.GetComponent<TextMesh>().richText = false;
+        text.GetComponent<TextMesh>().fontSize = 20;
     }
 
     private void OnDrawGizmos()
@@ -94,7 +93,7 @@ public class maw : MonoBehaviour
         Gizmos.color = dotColor;
         for (int i = 0; i < mesh.vertices.Length; i++)
         {
-            Gizmos.DrawSphere(transform.TransformPoint(mesh.vertices[i]), 0.03f);
+            Gizmos.DrawSphere(transform.TransformPoint(mesh.vertices[i]), 0.08f);
         }
     }
 
@@ -104,9 +103,8 @@ public class maw : MonoBehaviour
         if (radiusIn > radiusOut) { radiusIn = radiusOut / 2; }
         if (radiusIn < 0.1f) { radiusIn = 0.1f; }
         if (radiusOut < 0.2f) { radiusOut = 0.2f; }
-        mesh = new Mesh();
-        mesh.name = "polyhedron";
-        GetComponent<MeshFilter>().mesh = mesh;        
+
+        StartCoroutine(Generate());
     }
 
     // Update is called once per frame
@@ -117,6 +115,6 @@ public class maw : MonoBehaviour
 
     private void Awake()
     {
-      StartCoroutine(Generate());
+
     }
 }
