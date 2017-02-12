da43473b4d4838c6f421194a7b4b1f4689ac4902
Egor <dregor614@bk.ru>
2017-02-12 20:26:58 +0300

    need UV

--- Assets/scripts/maw.cs
+++ Assets/scripts/maw.cs
@@ -14,6 +14,8 @@ public class maw : MonoBehaviour
 
     public Color dotColor = new Color(150, 150, 150);
     private List<Vector3> allVertices = new List<Vector3>();
+    private List<Vector2> uv = new List<Vector2>();
+
     private Mesh mesh;
 
     private static IEnumerable<Vector3> polyhedron(float r, uint n, float z)
@@ -45,78 +47,97 @@ public class maw : MonoBehaviour
         text.GetComponent<TextMesh>().fontSize = 20;
     }
 
-    private IEnumerator Generate()
+    private void Generate()//private IEnumerator Generate()// 
     {
+        WaitForSeconds wait = new WaitForSeconds(0.001f);
         mesh = new Mesh();
         mesh.name = "polyhedron";
         GetComponent<MeshFilter>().mesh = mesh;
-        int[] triangles = new int[layers * angles * 12];
+        int[] triangles = new int[layers * angles * 12 + angles * 12];
         uint index = 0;
-        for (uint layer = 0; layer < layers; layer++)
+        for (uint layer = 0; layer < layers*2; layer++)
         {
             if (layer == 0)
             {
                 foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusIn))
                 {
-                    MakeAText(vertice, index.ToString());
                     allVertices.Add(vertice);
+                    uv.Add(new Vector2((float)vertice.x, (float)vertice.y));
+                    //MakeAText(vertice, index.ToString());
                     index++;
+                    mesh.vertices = allVertices.ToArray();
+                    //yield return false;
+                }
+            }
+            if (layer <= layers)
+            {
+                foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusOut))
+                {
+                    allVertices.Add(vertice);
+                    uv.Add(new Vector2((float)vertice.x, (float)vertice.y));
+                    //MakeAText(vertice, index.ToString());
+                    index++;
+                    mesh.vertices = allVertices.ToArray();
+                    //yield return false;
+                }
+                mesh.vertices = allVertices.ToArray();
+            }
+            if (layer >= layers)
+            {
+                uint antiLayer = layers;
+                if (layer != layers) { antiLayer = layers - (layer - layers); }
+
+                foreach (Vector3 vertice in layerOfPolyhedron(antiLayer, radiusIn))
+                {
+                    allVertices.Add(vertice);
+                    uv.Add(new Vector2((float)vertice.x, (float)vertice.y));
+                    //MakeAText(vertice, index.ToString());
+                    index++;
+                    mesh.vertices = allVertices.ToArray();
+                    //yield return false;
                 }
             }
 
-            foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusOut))
-            {
-                MakeAText(vertice, index.ToString());
-                allVertices.Add(vertice);
-                index++;
-            }
 
-            mesh.vertices = allVertices.ToArray();
-
-            yield return false;
-
+            
         }
-
-        for (uint layer = layers-1; layer > 0; layer--)
-        {
-            foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusIn))
-            {
-                MakeAText(vertice, index.ToString());
-                allVertices.Add(vertice);
-                index++;
-            }
-
-            mesh.vertices = allVertices.ToArray();
-            yield return false;
-        }
-
         mesh.vertices = allVertices.ToArray();
-        // MAKE face
+
         int start = 0;
         int j = 0;
-        for (int layer = 0; layer < layers*2 -1; layer++)
+        for (int layer = 0; layer <= layers*2 + 1; layer++)
         {
             for (int i = start; i < (start + (int)angles); i++)
             {
+
                 triangles[j] = i;
-                if (layer > layers * 2 - 1)
-                    triangles[j + 3] = triangles[j + 2] = i + (int)angles;
-                else
-                    triangles[j + 3] = triangles[j + 2] = i == start + angles - 1 ? start : i + 1;
+                triangles[j + 3] = triangles[j + 2] = i == start + angles - 1 ? start : i + 1;
 
-                if (layer < layers * 2 - 1)
+                if (layer < layers * 2 + 1)
+                {
                     triangles[j + 4] = triangles[j + 1] = i + (int)angles;
-                else
-                    triangles[j + 4] = triangles[j + 1] = i == start + angles - 1 ? start : i + 1;
-                triangles[j + 5] = i == start + angles - 1 ? start + (int)angles : i + (int)angles + 1; 
-                j += 6;
-                mesh.triangles = triangles;
-                yield return false;
-            }
-            start = start + (int)angles;
-        }
+                    triangles[j + 5] = i == start + angles - 1 ? start + (int)angles : i + (int)angles + 1;
+                }
+                else 
+                if (layer == layers * 2 + 1)
+                {
+                    triangles[j + 4] = triangles[j + 1] = i - start;
+                    triangles[j + 5] = i == start + angles - 1 ? 0 : i - start + 1;
+                }
 
+                //Debug.Log("layer: " + layer.ToString());
+                //Debug.Log(triangles[j].ToString() + triangles[j + 1].ToString() + triangles[j + 2].ToString());
+                //Debug.Log(triangles[j+3].ToString() + triangles[j + 4].ToString() + triangles[j + 5].ToString());
+                mesh.triangles = triangles;
+                j += 6;
+                //yield return wait;
+
+            }
+            start += (int)angles;
+        }
+        mesh.uv = uv.ToArray();
         mesh.RecalculateNormals();
+
 
     }
 
@@ -128,7 +149,7 @@ public class maw : MonoBehaviour
         Gizmos.color = dotColor;
         for (int i = 0; i < mesh.vertices.Length; i++)
         {
-            Gizmos.DrawSphere(transform.TransformPoint(mesh.vertices[i]), 0.08f);
+            Gizmos.DrawSphere(transform.TransformPoint(mesh.vertices[i]), 0.2f);
         }
     }
 
@@ -139,7 +160,8 @@ public class maw : MonoBehaviour
         if (radiusIn < 0.1f) { radiusIn = 0.1f; }
         if (radiusOut < 0.2f) { radiusOut = 0.2f; }
 
-        StartCoroutine(Generate());
+        //StartCoroutine(Generate());
+        Generate();
     }
 
     // Update is called once per frame
