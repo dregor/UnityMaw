6ea825161067c7cfbde09d16d19f110d25f46695
dregor <dregor614@bk.ru>
2017-02-15 17:23:45 +0300

    optimise vertice creating

--- Assets/scripts/maw.cs
+++ Assets/scripts/maw.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,10 +14,10 @@ public class maw : MonoBehaviour
 
     public Color dotColor = new Color(150, 150, 150);
     public float dotRadius = 0.03f;
+
     private List<Vector3> allVertices = new List<Vector3>();
     private List<Vector2> uv = new List<Vector2>();
-    private Texture texture;
-
+    private Texture2D texture;
     private Mesh mesh;
 
     private static IEnumerable<Vector3> polyhedron(float r, uint n, float z)
@@ -31,7 +31,16 @@ public class maw : MonoBehaviour
     private IEnumerable<Vector3> layerOfPolyhedron(uint layer, float radius)
     {
         float z = 0f;
-        if (layer > 0) { z = layer * depth / layers; }
+        if (layer <= 1)
+            z = 0;
+        else if (layer < layers + 1)
+            z = (layer - 1) * depth / layers;
+        else if (layer == layers + 1)
+            z = (layer - 2) * depth / layers;
+        else if (layer > layers + 1)
+            z = ( layers - (layer - layers) ) * depth / layers;
+        //Debug.Log(layer.ToString() + " - " + z);
+
         foreach (Vector3 vertice in polyhedron(radius, angles, z))
         {
             yield return vertice;
@@ -51,16 +60,18 @@ public class maw : MonoBehaviour
 
     private void Generate()//private IEnumerator Generate()// 
     {
-        WaitForSeconds wait = new WaitForSeconds(0.001f);
+        //WaitForSeconds wait = new WaitForSeconds(0.001f);
         mesh = new Mesh();
         mesh.name = "polyhedron";
         GetComponent<MeshFilter>().mesh = mesh;
-        int[] triangles = new int[layers * angles * 12 + angles * 12];
+        int[] triangles = new int[(layers+1) * angles * 12];
         uint index = 0;
+
+        float deltaRad = radiusOut - radiusIn;
 
         float height = texture.height;
         float oneProcH = height / 100;
-        float partH = height / (float)angles;
+        float partH = height / ((float)angles);
         float H = partH / oneProcH / 100;
 
         float width = texture.width;
@@ -68,67 +79,47 @@ public class maw : MonoBehaviour
         float partW = width / ((float)layers * 2);
         float W = partW / oneProcW / 100;
 
-        Debug.Log("width");
-        Debug.Log(width);
-        Debug.Log(W);
+        int boxSizeX = 10;
+        int boxSizeY = 10;
+        Color[] greenBox = new Color[boxSizeX*boxSizeY];
+        for (int i = 0; i < boxSizeX * boxSizeY; i++) { greenBox[i] = new Color(0, 200, 0); }
 
-        Debug.Log("height");
-        Debug.Log(height);
-        Debug.Log(H);
-        for (uint layer = 0; layer < layers*2; layer++)
+        Texture2D textureToSave = new Texture2D((int)width, (int)height, TextureFormat.RGB24, false);
+        textureToSave.SetPixels(texture.GetPixels());
+
+        for (uint layer = 0; layer < layers*2 ; layer++)
         {
-            if (layer == 0)
+            float radius = 0;
+            if ((layer == 0)||(layer > layers))
+                radius = radiusIn;
+            else if ((layer > 0)&&(layer<=layers))
+                radius = radiusOut;
+
+            foreach (Vector3 vertice in layerOfPolyhedron(layer, radius))
             {
-                foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusIn))
-                {
-                    allVertices.Add(vertice);
-                    uv.Add(new Vector2(W * (float)layer, H * ((float)index - layer * angles)));
-                    //MakeAText(vertice, index.ToString()+" - "+uv[uv.Count-1].x.ToString()+"/"+uv[uv.Count - 1].y.ToString());
-                    //MakeAText(vertice, index.ToString());
-                    index++;
-                    mesh.vertices = allVertices.ToArray();
-                    //yield return false;
-                }
-            }
-            if (layer <= layers)
-            {
-                foreach (Vector3 vertice in layerOfPolyhedron(layer, radiusOut))
-                {
-                    allVertices.Add(vertice);
-                    uv.Add(new Vector2(W * (float)layer, H * ((float)index - layer * angles)));
-                    //MakeAText(vertice, index.ToString()+" - "+uv[uv.Count - 1].x.ToString()+"/"+uv[uv.Count - 1].y.ToString());
-                    //MakeAText(vertice, index.ToString());
-                    index++;
-                    mesh.vertices = allVertices.ToArray();
-                    //yield return false;
-                }
+                allVertices.Add(vertice);
+                uv.Add(new Vector2(W * (float)layer, H * ((float)index - (layer + 1) * angles)));
+                textureToSave.SetPixels((int)((uint)partW * (layer)), (int)(partH * (index - (layer) * angles)), boxSizeX, boxSizeY, greenBox);
+                //Debug.Log(((uint)partW * layer).ToString() + " - " + (partH * (index - (layer + 1) * angles)).ToString());
+                //MakeAText(vertice, index.ToString()+" - "+uv[uv.Count - 1].x.ToString()+"/"+uv[uv.Count - 13].y.ToString());
+                //MakeAText(vertice, index.ToString());
+                index++;
                 mesh.vertices = allVertices.ToArray();
+                //yield return false;
             }
-            if (layer >= layers)
-            {
-                uint antiLayer = layers;
-                if (layer != layers) { antiLayer = layers - (layer - layers); }
-
-                foreach (Vector3 vertice in layerOfPolyhedron(antiLayer, radiusIn))
-                {
-                    allVertices.Add(vertice);
-                    uv.Add(new Vector2(W * (float)layer, H * ((float)index - layer * angles)));
-                    //MakeAText(vertice, index.ToString()+" - "+uv[uv.Count - 1].x.ToString()+"/"+uv[uv.Count - 13].y.ToString());
-                    //MakeAText(vertice, index.ToString());
-                    index++;
-                    mesh.vertices = allVertices.ToArray();
-                    //yield return false;
-                }
-            }
-
-
-            
+            //uv.Add(new Vector2(W * (float)layer, H * ((float)index - (layer + 1) * angles)));
         }
+       // uv.Add(new Vector2(W * ((float)layers*2), H * ((float)index - (((float)layers * 2) + 1) * angles)));
+
         mesh.vertices = allVertices.ToArray();
+
+        byte[] bytes = textureToSave.EncodeToJPG();
+        Destroy(textureToSave);
+        File.WriteAllBytes(Application.dataPath + "/../SavedImage.jpg", bytes);
 
         int start = 0;
         int j = 0;
-        for (int layer = 0; layer <= layers*2 + 1; layer++)
+        for (int layer = 0; layer <= layers * 2 -1; layer++)
         {
             for (int i = start; i < (start + (int)angles); i++)
             {
@@ -136,13 +127,13 @@ public class maw : MonoBehaviour
                 triangles[j] = i;
                 triangles[j + 3] = triangles[j + 2] = i == start + angles - 1 ? start : i + 1;
 
-                if (layer < layers * 2 + 1)
+                if (layer < layers * 2 -1)
                 {
                     triangles[j + 4] = triangles[j + 1] = i + (int)angles;
                     triangles[j + 5] = i == start + angles - 1 ? start + (int)angles : i + (int)angles + 1;
                 }
-                else 
-                if (layer == layers * 2 + 1)
+                else
+                if (layer == layers * 2 -1)
                 {
                     triangles[j + 4] = triangles[j + 1] = i - start;
                     triangles[j + 5] = i == start + angles - 1 ? 0 : i - start + 1;
@@ -160,10 +151,7 @@ public class maw : MonoBehaviour
         }
         mesh.uv = uv.ToArray();
         mesh.RecalculateNormals();
-
-
     }
-
 
 
     private void OnDrawGizmos()
@@ -176,18 +164,18 @@ public class maw : MonoBehaviour
         }
     }
 
-    // Use this for initialization
+
     void Start()
     {
         if (radiusIn > radiusOut) { radiusIn = radiusOut / 2; }
         if (radiusIn < 0.1f) { radiusIn = 0.1f; }
         if (radiusOut < 0.2f) { radiusOut = 0.2f; }
-        texture = gameObject.GetComponent<MeshRenderer>().material.mainTexture;  
+        texture = gameObject.GetComponent<MeshRenderer>().material.mainTexture as Texture2D;  
         //StartCoroutine(Generate());
         Generate();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
